a5f550d858318b7433804e3dcb1e66824e47930c
Andres Gonzalez Paez <andres.gonzalez.paez1@gmail.com>
2016-10-11 00:36:06 -0400

    newWearableProyect

---
+++ wear/src/main/java/demos/android/com/craneo/demowearable/MainActivity.java
@@ -0,0 +1,38 @@
+package demos.android.com.craneo.demowearable;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.wearable.view.WatchViewStub;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends Activity implements View.OnClickListener{
+
+    private TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
+        stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
+            @Override
+            public void onLayoutInflated(WatchViewStub stub) {
+                mTextView = (TextView) stub.findViewById(R.id.text);
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()){
+            case R.id.unknow:
+                break;
+            case R.id.confirm:
+                break;
+            default:
+                break;
+        }
+
+    }
+}
